24056f4b1e17e084557f4da0aa0499f111953139
Meysi Solano <meysi.solano01@gmail.com>
2025-06-27 14:34:48 -0600

    Vehiculo.java

--- src/Vehiculos/Vehiculo.java
+++ src/Vehiculos/Vehiculo.java
@@ -1,4 +1,23 @@
 package Vehiculos;
 
 public class Vehiculo {
+    protected String marca;
+    protected String modelo;
+    protected int anio;
+
+    public Vehiculo(String marca, String modelo, int anio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Año: " + anio);
+    }
+
+    public void arrancar() {
+        System.out.println("El vehículo está arrancando...");
+    }
 }
