50d79dbe59d97b8aa4440ae7a18c8e62d2270aea
Meysi Solano <meysi.solano01@gmail.com>
2025-06-27 14:36:45 -0600

    Camion.java

--- src/Vehiculos/Camion.java
+++ src/Vehiculos/Camion.java
@@ -1,4 +1,22 @@
 package Vehiculos;
 
-public class Camion {
+public class Camion extends Vehiculo {
+    private double capacidadCarga;
+
+    public Camion(String marca, String modelo, int anio, double capacidadCarga) {
+        super(marca, modelo, anio);
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.println("=== CAMIÓN ===");
+        super.mostrarDatos();
+        System.out.println("Capacidad de carga: " + capacidadCarga + " toneladas");
+    }
+
+    @Override
+    public void arrancar() {
+        System.out.println("El camión está arrancando con motor diésel.");
+    }
 }
