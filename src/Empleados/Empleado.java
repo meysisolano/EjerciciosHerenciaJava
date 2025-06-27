123490fd295c863828098719161ff58fff02ebb8
Meysi Solano <meysi.solano01@gmail.com>
2025-06-27 14:37:53 -0600

    Empleado.java

--- src/Empleados/Empleado.java
+++ src/Empleados/Empleado.java
@@ -1,4 +1,23 @@
 package Empleados;
 
 public class Empleado {
+    protected String nombre;
+    protected int id;
+    protected double salario;
+
+    public Empleado(String nombre, int id, double salario) {
+        this.nombre = nombre;
+        this.id = id;
+        this.salario = salario;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("ID: " + id);
+        System.out.println("Salario: $" + salario);
+    }
+
+    public double calcularSalario() {
+        return salario;
+    }
 }
