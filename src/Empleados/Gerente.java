aed42bb939d7d65be09d5142ab025dffde1da498
Meysi Solano <meysi.solano01@gmail.com>
2025-06-27 14:38:12 -0600

    Gerente.java

--- src/Empleados/Gerente.java
+++ src/Empleados/Gerente.java
@@ -1,4 +1,22 @@
 package Empleados;
 
-public class Gerente {
+public class Gerente extends Empleado {
+    private double bono;
+
+    public Gerente(String nombre, int id, double salario, double bono) {
+        super(nombre, id, salario);
+        this.bono = bono;
+    }
+
+    @Override
+    public void mostrarDatos() {
+        super.mostrarDatos();
+        System.out.println("Bono: $" + bono);
+        System.out.println("Salario total: $" + calcularSalario());
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salario + bono;
+    }
 }
