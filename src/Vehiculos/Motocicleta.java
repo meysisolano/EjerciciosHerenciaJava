9630fbae3a4bb340074e95dccd28636277787602
Meysi Solano <meysi.solano01@gmail.com>
2025-06-27 14:36:12 -0600

    Motocicleta.java

--- src/Vehiculos/Motocicleta.java
+++ src/Vehiculos/Motocicleta.java
@@ -1,16 +1,22 @@
-package Ejercicio1;
+package Vehiculos;
 
 public class Motocicleta extends Vehiculo {
-    boolean tieneSidecar;
+    private boolean tieneSidecar;
 
-    public Motocicleta(String marca, String modelo, boolean tieneSidecar) {
-        super(marca, modelo);
+    public Motocicleta(String marca, String modelo, int anio, boolean tieneSidecar) {
+        super(marca, modelo, anio);
         this.tieneSidecar = tieneSidecar;
     }
 
     @Override
     public void mostrarDatos() {
+        System.out.println("=== MOTOCICLETA ===");
         super.mostrarDatos();
-        System.out.println("Tiene sidecar: " + (tieneSidecar ? "Sí" : "No"));
+        System.out.println("¿Tiene sidecar?: " + (tieneSidecar ? "Sí" : "No"));
+    }
+
+    @Override
+    public void arrancar() {
+        System.out.println("La motocicleta está arrancando con fuerza.");
     }
 }
